d5ad0af545b17d43086489f1eee6d9ea0c46533f
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-26 00:11:49 +0200

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Day Of Dragons Egg Star-X"
+echo "(c) Copyright 2024 - SwitchCompagnie"
+
+echo "-------------------------------------------------------------------------------------------------"
+echo "Checking Updates"
+echo "-------------------------------------------------------------------------------------------------"
+apt update && apt install -y wget
+
+echo "-------------------------------------------------------------------------------------------------"
+echo "Starting POT Server"
+echo "-------------------------------------------------------------------------------------------------"
+./DOD/Binaries/Linux/DragonsServer-Linux-Shipping -log -SteamServerName=$SRV_NAME -Port=$SERVER_PORT -QueryPort=$QUERY_PORT
