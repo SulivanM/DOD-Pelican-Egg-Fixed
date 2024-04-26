e710010f9ef1962a0657e1a128de7f8bcea3f4c2
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-26 02:00:58 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,11 +3,6 @@ echo "Day Of Dragons Egg Star-X"
 echo "(c) Copyright 2024 - SwitchCompagnie"
 
 echo "-------------------------------------------------------------------------------------------------"
-echo "Checking Updates"
+echo "Starting DOD Server & Checking Updates"
 echo "-------------------------------------------------------------------------------------------------"
-apt update && apt install -y wget
-
-echo "-------------------------------------------------------------------------------------------------"
-echo "Starting POT Server"
-echo "-------------------------------------------------------------------------------------------------"
-./DragonsServer.sh -log -SteamServerName=$SRV_NAME -Port=$SERVER_PORT -QueryPort=$QUERY_PORT -force_install_dir=/DOD
+./DragonsServer.sh -log -SteamServerName=$SRV_NAME -Port=$SERVER_PORT -QueryPort=$QUERY_PORT
