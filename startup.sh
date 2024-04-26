42e127e7b30944bbc811f5a943755731a4151b95
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-26 16:38:45 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ echo "(c) Copyright 2024 - SwitchCompagnie"
 echo "-------------------------------------------------------------------------------------------------"
 echo "Starting DOD Server & Checking Updates"
 echo "-------------------------------------------------------------------------------------------------"
-./DragonsServer.sh ${MAP} -log -iServerType=$SERVER_TYPE -SteamServerName=$SRV_NAME -sServerMOTD=$MOTD -iMaxPlayers=$MAX_PLAYER -Port=$SERVER_PORT -QueryPort=$QUERY_PORT
+./DragonsServer.sh -Port={{SERVER_PORT}} -QueryPort={{QUERY_PORT}}  -SteamServerName=\"{{SRV_NAME}}\" -log
