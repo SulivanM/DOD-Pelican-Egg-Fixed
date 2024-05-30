cbcfc8076365dfc7a78ae923c250d53201684eda
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-05-30 22:37:28 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ echo "(c) Copyright 2024 - SwitchCompagnie"
 echo "-------------------------------------------------------------------------------------------------"
 echo "Starting DOD Server & Checking Updates"
 echo "-------------------------------------------------------------------------------------------------"
-./DragonsServer.sh ${MAP} -log -SteamServerName=$SRV_NAME -Port=$SERVER_PORT -QueryPort=$QUERY_PORT
+./DragonsServer.sh -log -SteamServerName=$SRV_NAME -Port=$SERVER_PORT -QueryPort=$QUERY_PORT
